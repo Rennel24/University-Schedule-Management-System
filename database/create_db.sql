d8e9674556d27eb4597b4a0c9ef9b098d1192091
Rennel24 <24-00424@g.batstate-u.edu.ph>
2025-12-14 19:51:58 +0800

    deleted create_db file

--- database/create_db.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO sections (section_name, program_id) VALUES
-('BSCS 1101', (SELECT program_id FROM programs WHERE program_name='BSCS')),
-('BSCS 2101', (SELECT program_id FROM programs WHERE program_name='BSCS')),
-('BSIT 3101', (SELECT program_id FROM programs WHERE program_name='BSIT')),
-('BSCS 3101', (SELECT program_id FROM programs WHERE program_name='BSCS')),
-('BSIT 4101', (SELECT program_id FROM programs WHERE program_name='BSIT')),
-('BSCS 4101', (SELECT program_id FROM programs WHERE program_name='BSCS'));
